74854ada569288b394d6c1f10b86fb784ff6d22a
Juan Manuel Eraso <juaneraso2003@hotmail.com>
2026-06-17 10:27:40 -0500

    Vigesimo Sexto ejercicio APOII reorganizado

---
+++ src/context/Main.java
@@ -0,0 +1,17 @@
+package context;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Mensaje
+        System.out.println("Hola Mundo");
+
+        // Cierre Scanner
+        sc.close();
+    }
+}
